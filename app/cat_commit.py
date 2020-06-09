3e16516f8757c1fcafedf1abde9311abd19165ac
Raghav Nair <nairraghav@hotmail.com>
2020-06-08 19:28:17 -0700

    using some bootstrap basics and removing debug print

--- app/cat_commit.py
+++ app/cat_commit.py
@@ -1,8 +1,6 @@
 
 class CatCommit:
     def __init__(self, commit_item):
-        print(commit_item)
-
         try:
             self.url = commit_item.get('html_url')
             self.author_image_url = commit_item.get('author').get('avatar_url')
