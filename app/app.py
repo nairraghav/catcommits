483a725cc10aba70e09f0ae0ff31f0ab065f5b1d
Raghav Nair <nairraghav@hotmail.com>
2020-06-09 21:00:10 -0700

    Adding pagination

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 import requests
 import requests_cache
 from app.cat_commit import CatCommit
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -10,12 +10,14 @@ requests_cache.install_cache('github_cache', backend='sqlite', expire_after=180)
 
 @app.route('/', methods=["GET"])
 def home():
-    github_cat_search_url = "https://api.github.com/search/commits?q=cat"
-    headers = {"Accept": "application/vnd.github.cloak-preview"}
-    cat_search_response = requests.get(url=github_cat_search_url, headers=headers)
-    cat_commits = []
-    for item in cat_search_response.json()['items']:
-        cat_commit = CatCommit(item)
-        if cat_commit.good_commit:
-            cat_commits.append(cat_commit)
-    return render_template('home.html', commits=cat_commits)
+    page_number = int(request.args.get("page_number", default=1))
+    if page_number >= 1:
+        github_cat_search_url = f"https://api.github.com/search/commits?q=cat&page={page_number}"
+        headers = {"Accept": "application/vnd.github.cloak-preview"}
+        cat_search_response = requests.get(url=github_cat_search_url, headers=headers)
+        cat_commits = []
+        for item in cat_search_response.json()['items']:
+            cat_commits.append(CatCommit(item))
+        return render_template('home.html', commits=cat_commits, page_number=page_number)
+    else:
+        return render_template('home.html', page_number=page_number)
--- app/cat_commit.py
+++ app/cat_commit.py
@@ -1,10 +1,16 @@
 
 class CatCommit:
     def __init__(self, commit_item):
+        self.url = commit_item.get('html_url')
+        self.message = commit_item.get('commit').get('message')
         try:
-            self.url = commit_item.get('html_url')
             self.author_image_url = commit_item.get('author').get('avatar_url')
-            self.message = commit_item.get('commit').get('message')
-            self.good_commit = True
         except:
-            self.good_commit = False
+            self.author_image_url= "https://github.githubassets.com/images/modules/open_graph/github-octocat.png"
+
+    def serialize(self):
+        return {
+            'url': self.url,
+            'author_image_url': self.author_image_url,
+            'message': self.message,
+        }
