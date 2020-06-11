ac38149f698e234b5ffa8055c79b0c025a8ad1d7
Raghav Nair <nairraghav@hotmail.com>
2020-06-10 18:18:19 -0700

    Adding more logic to show errors, giving helper hints, removing pagination if prev/next don't exist

--- commits/app.py
+++ commits/app.py
@@ -11,13 +11,15 @@ requests_cache.install_cache('github_cache', backend='sqlite', expire_after=1080
 @app.route('/', methods=["GET"])
 def home():
     page_number = int(request.args.get("page_number", default=1))
-    if page_number >= 1:
+    if 0 < page_number < 35:  # we are not allowed search beyond 1000 results
         github_cat_search_url = f"https://api.github.com/search/commits?q=cat&page={page_number}"
         headers = {"Accept": "application/vnd.github.cloak-preview"}
         cat_search_response = requests.get(url=github_cat_search_url, headers=headers)
         cat_commits = []
-        for item in cat_search_response.json().get('items'):
-            cat_commits.append(CatCommit(item))
-        return render_template('home.html', commits=cat_commits, page_number=page_number)
-    else:
-        return render_template('home.html', page_number=page_number)
+        items = cat_search_response.json().get('items')
+        if items:
+            print(f"{page_number} has items!")
+            for item in items:
+                cat_commits.append(CatCommit(item))
+            return render_template('home.html', commits=cat_commits, page_number=page_number, no_kitties=False)
+    return render_template('home.html', page_number=page_number, no_kitties=True)
